2b42331582d75091a4cb37bcfe098a317b09a2a6
aditya-sutar-45 <aditya1.sutar@outlook.com>
2026-02-07 15:46:28 +0530

    increased the sample code and test cases for testing (worker pool next)

--- verificationEngine/cmd/verification-engine/main.go
+++ verificationEngine/cmd/verification-engine/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"time"
 
 	"github.com/ShashwatAwate/endpointX/verificationEngine/internal/app"
 )
 
 func main() {
-	if err := app.Verify(); err != nil {
-		log.Fatalln(err)
+	start := time.Now()
+	results := app.Start()
+
+	fmt.Println("\nDone in: ", time.Since(start))
+	fmt.Println("number of results: ", len(results))
+
+	for _, result := range results {
+		fmt.Println(result.Status)
 	}
 }
--- verificationEngine/internal/app/verify.go
+++ verificationEngine/internal/app/verify.go
@@ -2,42 +2,62 @@
 package app
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/ShashwatAwate/endpointX/verificationEngine/internal/engine"
 	"github.com/ShashwatAwate/endpointX/verificationEngine/internal/models"
 )
 
-func Verify() error {
-	spec, err := models.NewSpec("./sample.json")
+func Start() []*models.Result {
+	seeds := []string{
+		"./seeds/sample_1.json",
+		"./seeds/sample_2.json",
+		"./seeds/sample_3.json",
+		"./seeds/sample_4.json",
+		"./seeds/sample_5.json",
+	}
+
+	results := []*models.Result{}
+
+	for i, seed := range seeds {
+		result, err := Verify(seed)
+		fmt.Println("done verifying seed: ", i+1)
+		if err != nil {
+			fmt.Println("error verifying seed: ", i+1)
+			fmt.Println("error: ", err)
+		}
+		results = append(results, result)
+	}
+
+	return results
+}
+
+func Verify(jsonPath string) (*models.Result, error) {
+	spec, err := models.NewSpec(jsonPath)
 	if err != nil {
-		return fmt.Errorf("error making spec: %w", err)
+		return nil, fmt.Errorf("error making spec: %w", err)
 	}
 
 	w, err := models.NewWorkspace(spec)
 	if err != nil {
-		return fmt.Errorf("error making workspace: %w", err)
+		return nil, fmt.Errorf("error making workspace: %w", err)
 	}
 	if err := w.Init(); err != nil {
-		return fmt.Errorf("error initializing the workspace: %w", err)
+		return nil, fmt.Errorf("error initializing the workspace: %w", err)
 	}
 	defer w.Clean()
 
 	result, err := engine.RunTests(w)
 	if err != nil {
 		// gg ho gaya
-		return fmt.Errorf("engine error: %w", err)
+		return nil, fmt.Errorf("engine error: %w", err)
 	}
 
 	if result.Status == models.TestPassed {
-		fmt.Print("TEST PASSED!!!\n\n")
+		fmt.Print("TEST PASSED!!!\n")
 	} else {
-		fmt.Print("test failed :(\n\n")
+		fmt.Print("test failed :(\n")
 	}
 
-	b, _ := json.MarshalIndent(result, "", "  ")
-	fmt.Println(string(b))
-
-	return nil
+	return result, nil
 }
