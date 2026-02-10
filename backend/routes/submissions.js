65eb2ce02b3edcca26af8d05dabba23d80c937c1
Shashwat Awate <shashwat.awate@gmail.com>
2026-02-10 19:42:09 +0530

    added db.js code

--- backend/routes/submissions.js
+++ backend/routes/submissions.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const pool = require('../db');
 const router = express.Router();
 
 
 // /submission/:questionId
 router.get("/:questionId", async (req, res) => {
+ const {questionId} = req.params;
+ const userId = req.userId;
+
+ const timeoutMS = 250000;
+ const pollIntervalMS = 500;
+
+ const start = Date.now();
+
+ try{
+    while(Date.now() - start < timeoutMS){
+        
+    }
+ }
+ catch{
+
+ }
 })
 
 
