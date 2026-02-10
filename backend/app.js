179cd47eccfa217e8ee0c20ee71897fb5f113c2d
aditya-sutar-45 <aditya1.sutar@outlook.com>
2026-02-10 19:04:59 +0530

    idk

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
- const app = express();
+const app = express();
 const authRoutes = require('./routes/auth');
 const protectedRoute = require('./routes/protectedRoute');
 const questionsRoutes = require('./routes/questions');
- app.use(express.json());
+require("dotenv").config();
+app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/protected', protectedRoute);
 app.use('/questions', questionsRoutes);
- const PORT = process.env.PORT || 3000;
- app.listen(PORT, () => {
- console.log(`Server is running on port ${PORT}`);
- });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
